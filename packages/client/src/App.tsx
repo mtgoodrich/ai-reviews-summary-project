35060e5f2ee7f499a4ddc6e97ce414cabd753f27
Matthew Goodrich <matthew@propertylens.com>
2025-09-04 22:53:00 -0700

    Move to packages dir, Add vite frontend, concurrent

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,11 @@
+import ReviewList from "./components/reviews/ReviewList";
+
+function App() {
+    return (
+        <div className="px-4 w-full">
+            <ReviewList productId={1} />
+        </div>
+    );
+}
+
+export default App;
